32d9e37a98be98f60f324babdaad37c350fbc984
Oleg Mullakaev <o.mullakaev@gmail.com>
2018-01-14 03:07:04 +0300

    start vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  #app test
+    h1 hello world!!!
+    h2 Всем привет
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  h1 {
+    color: $blue;
+    font-size: rem(20px);
+  }
+  h2 {
+    background: green;
+  }
+</style>
