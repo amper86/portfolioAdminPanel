43c32e379a192b6efde08db22d522b672c88f8e2
Oleg Mullakaev <o.mullakaev@gmail.com>
2018-01-16 02:19:40 +0300

    add layout all tabs

--- src/components/blog/blog.vue
+++ src/components/blog/blog.vue
@@ -1,10 +1,35 @@
 <template lang="pug">
-    h1 привет блог
+  .blog
+    h2.title Страница "Блог"
+    .container
+      .form.col
+        h3.form-title Добавить запись
+        .row
+          app-input(
+          type="text",
+          placeholder="Название"
+          )
+        .row
+          app-input(
+          type="date"
+          )
+        .row
+          textarea.input.blog-textarea(
+          type="text",
+          placeholder="Содержание"
+          )
+        .row
+          app-button(
+          title="Добавить"
+          )
 </template>
 
 <script>
     export default {
-
+      components: {
+        AppButton: require('_common/button/button.vue'),
+        AppInput: require('_common/input/input.vue')
+      }
     }
 </script>
 
--- src/components/works/works.vue
+++ src/components/works/works.vue
@@ -1,11 +1,51 @@
 <template lang="pug">
-  h1 Works components
+  .works
+    h2.title Страница "Мои работы"
+    .form
+      h3.form-title Добавить работу
+      .row
+        app-input(
+        placeholder="Название проекта"
+        type="text"
+        )
+      .row
+        app-input(
+        placeholder="Технологии"
+        type="text"
+        )
+      .row
+        label.upload
+          input.type-file(
+          type="file",
+          )
+          .upload__icon
+          .upload__text Загрузить картинку
+        div.error-message {{validation.firstError('fields.file')}}
+      .row
+        app-button(
+        title="Добавить",
+          :disabled="validation.hasError('fields.file')",
+        )
 </template>
 
 <script>
-    export default {
+  import { Validator } from 'simple-vue-validator'
 
+  export default {
+    mixins: [require('simple-vue-validator').mixin],
+    validators: {
+      'fields.file': (value) => {
+        return Validator.custom(() => {
+          if (Validator.isEmpty(value))
+            return 'Загрузите картинку'
+        })
+      }
+    },
+    components: {
+      AppButton: require('_common/button/button.vue'),
+      AppInput: require('_common/input/input.vue')
     }
+  }
 </script>
 
 <style src="./works.scss" lang="scss" scoped>
